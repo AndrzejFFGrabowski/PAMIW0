c945e355790ef796c62ecb0aa272867162b2ee86
AndrzejFFGrabowski <Andrzej.f.f.grabowski@gmail.com>
2022-11-25 01:36:23 +0100

    e

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ from uuid  import uuid4
 from bcrypt import checkpw
 from authentication import authenticated_users, authenticate
 from homepageLogic import generateSite, searchLogic
-from sqlLogic import generateSqlSite, fuc
+from sqlLogic import generateSqlSite, fuc, addProductToDB, showProducts
 from dotenv import load_dotenv
 from os import getenv
 
@@ -41,6 +41,24 @@ def search():
 @app.route("/sql", methods=["GET"])
 def generateSql():
     return generateSqlSite()
+
+@app.route('/with-json', methods=["POST"])
+def with_json():
+    data = request.get_json(silent=True)
+    if data:
+    	ID = data.get("id","nothing")
+    	name = data.get("product", "nothing")
+    	addProductToDB()
+    	return "", 200
+    else:
+    	ID = request.form.get("id","nothing")
+    	name = request.form.get("product","nothing")
+    	addProductToDB()
+    	return "",200
     
+    
+@app.route("/showProducts", methods=["GET"])
+def showProductsList():
+    return showProducts()    
 #if __name__ == '__main__':
 #    app.run(host="192.168.1.122", port=5050)
--- app/sqlLogic.py
+++ app/sqlLogic.py
@@ -1,7 +1,60 @@
 from flask import make_response, render_template
+import sqlite3
+from sqlite3 import Error
 
 def generateSqlSite():
+    create_connection("pythonDB.db",""" CREATE TABLE IF NOT EXISTS products (
+                                        id integer PRIMARY KEY,
+                                        name text NOT NULL
+                                    ); """)
     return render_template("sql.html")
 
 def fuc():
     return "yes"
+    
+
+
+
+def create_connection(db_file,sql):
+    """ create a database connection to a SQLite database """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        print(sqlite3.version)
+        print("it's alive")
+        print(sql)
+    except Error as e:
+        print(e)
+    finally:
+        if conn:
+            msg=executeSql(conn,sql)
+            print(executeSql(conn,"SELECT * FROM products;"))
+            conn.close()
+            return msg
+
+
+def addProductToDB():
+    create_connection("pythonDB.db",""" INSERT INTO products
+                                        (id, name)
+                                        VALUES
+                                        (1, "strawbery")""")
+    return generateSqlSite()
+                                    
+def showProducts():
+    msg=create_connection("pythonDB.db","""SELECT * FROM products;""")
+    text=""
+    for row in msg:
+    	text=text+row
+    return text
+    
+def executeSql(conn, sql):
+    try:
+    	c = conn.cursor()
+    	c.execute(sql)
+    	msg=c.fetchall()
+    	return msg
+    except Error as e:
+    	print(e)
+    	return "Server Error 501"
+    	
+    
